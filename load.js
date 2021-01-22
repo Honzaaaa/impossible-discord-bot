45f002f8186bb12f666672e610d15a12d73efa93
Honza <70645577+Honzaaaa@users.noreply.github.com>
2021-01-22 20:44:30 +0100

    Update load.js

--- load.js
+++ load.js
@@ -2,28 +2,27 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = (client) => {
-  const baseFile = 'command-base.js'
-  const commandBase = require(`./${baseFile}`)
+  const handler = require(`./handler.js`)
 
-  const commands = []
+  const cmd = []
 
-  const readCommands = (dir) => {
-    const files = fs.readdirSync(path.join(__dirname, dir))
-    for (const file of files) {
-      const stat = fs.lstatSync(path.join(__dirname, dir, file))
+  const readCmd = (dir) => {
+    const f = fs.readdirSync(path.join(__dirname, dir))
+    for (const cmd of f) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, cmd))
       if (stat.isDirectory()) {
-        readCommands(path.join(dir, file))
-      } else if (file !== baseFile && file !== 'load-commands.js') {
-        const option = require(path.join(__dirname, dir, file))
-        commands.push(option)
+        readCmd(path.join(dir, cmd))
+      } else if (cmd !== handler && cmd !== 'load-commands.js') {
+        const option = require(path.join(__dirname, dir, cmd))
+        cmd.push(option)
         if (client) {
-          commandBase(client, options)
+          handler(client, options)
         }
       }
     }
   }
 
-  readCommands('./cmds')
+  readCmd('./cmds')
 
-  return commands
+  return cmd
 }
