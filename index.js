54337a14f3cea331a19bc3a7bf2b28d0bfb1c317
Honza <70645577+Honzaaaa@users.noreply.github.com>
2021-01-22 17:43:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const colors = require("colors");
 const title = require("console-title");
+const fs = require("fs")
 
 // Creating client
 const client = new Discord.Client();
@@ -23,3 +24,17 @@ const fulltime = ´${hou}:${min}:${sec}´;
 const fulldate = ´${day}:${mon}:${yea}´;
 
 // Now, the code.
+client.login(config.token);
+
+client.on("ready", () => {
+  client.user.setPresence("Loading...")
+  console.log(´[${fulltime}] | Loading...´)
+})
+
+client.on("ratelimit", () => {
+  client.user.setPresence("Being rate limited! Please wait 5 seconds.")
+  console.log(´[${fulltime}] | Rate limit warning! Freezing to 5 seconds.´)
+  setTimeout(() => {
+     return
+  }, 5000)
+})
